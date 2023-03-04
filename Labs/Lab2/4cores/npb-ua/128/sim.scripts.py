a15d73c825fdd9f2e48153f6f0354258696dac81
Amy Bui <amybui0602@gmail.com>
2023-03-03 19:41:37 -0500

    lab2 partial data

---
+++ Labs/Lab2/4cores/npb-ua/128/sim.scripts.py
@@ -0,0 +1,5 @@
+import sys
+sys.argv = [ "/h/abui02/sniper-container/sniper-7.3/scripts/periodic-stats.py", "1000:2000" ]
+execfile("/h/abui02/sniper-container/sniper-7.3/scripts/periodic-stats.py")
+sys.argv = [ "/h/abui02/sniper-container/sniper-7.3/scripts/markers.py", "markers" ]
+execfile("/h/abui02/sniper-container/sniper-7.3/scripts/markers.py")
