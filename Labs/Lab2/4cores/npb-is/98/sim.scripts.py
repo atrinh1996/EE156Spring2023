c1b2b5521d92a1a859266411adbdef8559f096bb
Amy Bui <amybui0602@gmail.com>
2023-03-03 19:57:28 -0500

    remove lab2 incomplete data

--- Labs/Lab2/4cores/npb-is/98/sim.scripts.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.argv = [ "/h/abui02/sniper-container/sniper-7.3/scripts/periodic-stats.py", "1000:2000" ]
-execfile("/h/abui02/sniper-container/sniper-7.3/scripts/periodic-stats.py")
-sys.argv = [ "/h/abui02/sniper-container/sniper-7.3/scripts/markers.py", "markers" ]
-execfile("/h/abui02/sniper-container/sniper-7.3/scripts/markers.py")
